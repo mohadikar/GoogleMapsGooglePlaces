e299d424e7cf248f6acd837458fc3d3e69c019f7
Vinay <vinaymohadikar.96@gmail.com>
2018-06-30 20:40:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/googlemapsgoogleplaces/models/PlaceInfo.java
@@ -0,0 +1,77 @@
+package com.example.hp.googlemapsgoogleplaces.models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class PlaceInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private LatLng latlng;
+
+
+    public PlaceInfo(String name, String address, String phoneNumber,String email,
+                     LatLng latlng) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email=email;
+        this.latlng = latlng;
+
+    }
+
+    public PlaceInfo() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LatLng getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(LatLng latlng) {
+        this.latlng = latlng;
+    }
+
+    public String toString() {
+        return "PlaceInfo{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", latlng=" + latlng +
+                '}';
+    }
+}
